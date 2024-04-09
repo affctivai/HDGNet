f513f69b870c948b6dece5c3f5cf6259ea1a8444
lhy <migalihanyu@gmail.com>
2024-04-09 17:59:28 +0900

    update the explanation

--- Affectiv_AI/examples/HDGNet_examples.sh
+++ Affectiv_AI/examples/HDGNet_examples.sh
@@ -1,7 +1,7 @@
 # Tell the Python interpreter that this directory is the root of your source code by setting the environment variable
 windows: set PYTHONPATH=your_directory/Affectiv_AI/;%PYTHONPATH%
 linux: export PYTHONPATH=your_directory/Affectiv_AI/:$PYTHONPATH
-
+if you want to cancel it in linux: unset PYTHONPATH
 
 # SEED, if you not need to split dataset, please add "--Split=True"
 reference:
@@ -10,7 +10,6 @@ python -m HDGNet --dataset_name=SEED --num_classes=3 --n_outer=5 --n_inner=2 --g
 
 # SEED-IV
 python -m HDGNet --dataset_name=SEED-IV --num_classes=4 --n_outer=3 --n_inner=2 --graph_defi=POSTERIOR
-
 # SEED-V
 python -m HDGNet --dataset_name=SEED-V --num_classes=5 --n_outer=3 --n_inner=2 --graph_defi=POSTERIOR
 
